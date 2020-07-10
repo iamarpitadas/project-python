17d78068b2154332525882a4575266fbad2643c9
iamarpitadas <68124409+iamarpitadas@users.noreply.github.com>
2020-07-11 00:09:35 +0530

    Created hello.py
    This file contains programs to find the area of a circle using radius and finding file extension.

---
+++ hello.py
@@ -0,0 +1,14 @@
+# Area of circle using radius
+x=input("Enter the radius of the circle :")
+r=int(x)
+a=3.14*r**2
+print("Area of the circle",a)
+
+
+# print file extension
+filename=input('Enter a filename: ')
+index=0
+for i in range(len(filename)):
+    if filename[i]=='.':
+       index=i
+print(filename[index+1: ])
