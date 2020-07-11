da098ec6061a69671f5ae69044702157ad98cd57
iamarpitadas <68124409+iamarpitadas@users.noreply.github.com>
2020-07-11 18:07:30 +0530

    created hello.py
    This file contains the program to find the area of circle using its radius.

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 # Area of circle using radius
-x=input("Enter the radius of the circle :")
-r=int(x)
-a=3.14*r**2
-print("Area of the circle",a)
 
+x=input("Enter the radius of the circle :")
+
+r=float(x)
+
+a=3.14159*r**2
+
+print("Area of the circle: ",a)
 
